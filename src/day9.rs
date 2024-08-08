2e1ff0e339f9dd2a1fde80320631880f4318bd8a
binotation <68807919+binotation@users.noreply.github.com>
2024-08-09 01:40:42 +1000

    day9

--- src/day9.rs
+++ src/day9.rs
@@ -5,7 +5,10 @@ use rustc_hash::FxHashMap;
 /// Use a variation of the dynamic programming approach to solving the Hamiltonian path problem.
 /// https://www.geeksforgeeks.org/hamiltonian-path-using-dynamic-programming/
 #[allow(dead_code)]
-fn length_of_shortest_hamiltonian_path(adj: Vec<Vec<u32>>) -> u32 {
+fn length_hamiltonian_paths<F>(adj: Vec<Vec<u32>>, comparator: F) -> Vec<Vec<Option<u32>>>
+where
+    F: Fn(u32, u32) -> u32,
+{
     let n: usize = adj.len();
     // Represents length of shortest Hamiltonian path of each subset of nodes ending with node i
     let mut dp = vec![vec![None; n]; 1 << n];
@@ -28,7 +31,7 @@ fn length_of_shortest_hamiltonian_path(adj: Vec<Vec<u32>>) -> u32 {
                 if dp[subset ^ (1 << node)][neighbor].is_some() {
                     // Update length of path ending with node if neighbor path + dist[neighbor][node] is shorter
                     if let Some(d) = dp[subset][node] {
-                        dp[subset][node] = Some(std::cmp::min(
+                        dp[subset][node] = Some(comparator(
                             d,
                             dp[subset ^ (1 << node)][neighbor].unwrap() + adj[neighbor][node],
                         ));
@@ -40,12 +43,7 @@ fn length_of_shortest_hamiltonian_path(adj: Vec<Vec<u32>>) -> u32 {
             }
         }
     }
-    // Retrieve smallest path length from subset containing all nodes
-    dp[(1 << n) - 1]
-        .iter()
-        .map(|v| v.unwrap()) // Path ending with all nodes will exist
-        .min()
-        .unwrap()
+    dp
 }
 
 /// Build the adjacency matrix adj. This tells us the distance between every node.
@@ -67,18 +65,18 @@ fn build_adjacency_matrix(locations: &str) -> Vec<Vec<u32>> {
         let distance: u32 = line_split.next().unwrap().parse::<u32>().unwrap();
 
         // Assign location name an index if unassigned
-        if locations_index.get(location1).is_none() {
+        if !locations_index.contains_key(location1) {
             locations_index.insert(location1, index);
             // Add row into adj, column is not needed because of the special ordering of the input text
             adj.push(vec![0; index + 1]);
             index += 1;
         }
-        if locations_index.get(location2).is_none() {
+        if !locations_index.contains_key(location2) {
             locations_index.insert(location2, index);
             // Add row and column into adj
             adj.push(vec![0; index + 1]);
-            for i in 0..index {
-                adj[i].push(0);
+            for row in adj.iter_mut().take(index) {
+                row.push(0);
             }
             index += 1;
         }
@@ -101,12 +99,43 @@ mod solution {
     fn get_length_of_shortest_hamiltonian_path() {
         let locations = get_input("locations").unwrap();
         let adj = build_adjacency_matrix(&locations);
-        assert_eq!(length_of_shortest_hamiltonian_path(adj), 141);
+        let n = adj.len();
+        let dp = length_hamiltonian_paths(adj, std::cmp::min);
+        // Retrieve shortest path length from subset containing all nodes
+        let length_shortest_path = dp[(1 << n) - 1]
+            .iter()
+            .map(|v| v.unwrap()) // Path ending with all nodes will exist
+            .min()
+            .unwrap();
+        assert_eq!(length_shortest_path, 141);
+    }
+
+    #[test]
+    fn get_length_of_longest_hamiltonian_path() {
+        let locations = get_input("locations").unwrap();
+        let adj = build_adjacency_matrix(&locations);
+        let n = adj.len();
+        let dp = length_hamiltonian_paths(adj, std::cmp::max);
+        // Retrieve longest path length from subset containing all nodes
+        let length_longest_path = dp[(1 << n) - 1]
+            .iter()
+            .map(|v| v.unwrap()) // Path ending with all nodes will exist
+            .max()
+            .unwrap();
+        assert_eq!(length_longest_path, 736);
     }
 
     #[test]
     fn test_length_of_shortest_hamiltonian_path_simple() {
         let adj = vec![vec![0, 464, 518], vec![464, 0, 141], vec![518, 141, 0]];
-        assert_eq!(length_of_shortest_hamiltonian_path(adj), 605);
+        let n = adj.len();
+        let dp = length_hamiltonian_paths(adj, std::cmp::min);
+        // Retrieve shortest path length from subset containing all nodes
+        let length_shortest_path = dp[(1 << n) - 1]
+            .iter()
+            .map(|v| v.unwrap()) // Path ending with all nodes will exist
+            .min()
+            .unwrap();
+        assert_eq!(length_shortest_path, 605);
     }
 }
